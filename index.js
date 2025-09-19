d296a3e63a32726cd662262eea3dbe93bc9a7f9a
ErikaNagy <50984976+ErikaNagy@users.noreply.github.com>
2025-09-19 15:02:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = document.getElementById("homescore")
+let guestScore = document.getElementById("guestscore")
+
+let homeCount =0
+let guestCount =0
+
+
+function homeincrement() {
+    homeCount =homeCount+1
+    homeScore.innerText=homeCount
+    console.log(homeCount)
+}
+
+function guestincrement() {
+    guestCount =guestCount+1
+    guestScore.innerText=guestCount
+    console.log(guestCount)
+}
+
+function homeincrement2() {
+    homeCount =homeCount+2
+    homeScore.innerText=homeCount
+    console.log(homeCount)
+}
+
+function guestincrement2() {
+    guestCount =guestCount+2
+    guestScore.innerText=guestCount
+    console.log(guestCount)
+}
+
+function homeincrement3() {
+    homeCount =homeCount+3
+    homeScore.innerText=homeCount
+    console.log(homeCount)
+}
+
+function guestincrement3() {
+    guestCount =guestCount+3
+    guestScore.innerText=guestCount
+    console.log(guestCount)
+}
